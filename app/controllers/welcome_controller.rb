b49bdc7e4667f95d6bf2f7c4d247ce8fbb743766
Danilo Freire <danilofreirea90@gmail.com>
2024-12-06 12:29:02 -0300

    Usando bootstrap para criação do layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
+  layout 'application'
+
   def index
-    @carros = Carro.all
+    @carros = Carro.all.order(updated_at:
+    :desc).limit(6)
   end
 end
+
